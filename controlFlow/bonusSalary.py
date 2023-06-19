74483779aadbe427e3bb009681b25434a73558ad
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-06-19 14:09:25 -0500

    Create bonusSalary.py

---
+++ controlFlow/bonusSalary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# take float input for salary
+salary = float(input())
+
+# take integer input for years
+years = float(input())
+
+# check if years worked is greater than 5 or not
+if(years > 5): 
+    # calculate bonus using 5 * salary / 100
+    bonus  = 5 * salary / 100
+    # print bonus
+    print(bonus)
