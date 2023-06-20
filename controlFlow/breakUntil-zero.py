7c390a19db6a5dfce297d038e87083b35fd3bd6b
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-06-20 16:43:02 -0500

    finish control flow part

---
+++ controlFlow/breakUntil-zero.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# run a while loop that is always true
+while(True):
+    # take input for number
+    number = int(input())
+    
+    # terminate the loop if the user input is 0
+    if number == 0:
+        break
+    # print the number
+    print(number)
---
+++ controlFlow/factorsNumbers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+number = int(input())
+
+# create a for loop that iterates from 1 to number
+for i in range (1, number + 1):
+    # check if number is perfectly divisible (remainder 0)
+    # If yes, print i
+    if number % i == 0:
+        print(i)
